32071656436d407acc206bf87a1d5c83ebe9276d
elvis <elvissomers98@gmail.com>
2023-05-12 14:43:16 +0200

    Pawn class bugfixes

--- src/classes/pieces/Pawn.java
+++ src/classes/pieces/Pawn.java
@@ -25,31 +25,33 @@ public class Pawn extends Piece{
         int yPos = this.getSquare().getVerticalPosition();
         int xSize = this.getBoard().getHorizontalSize();
         int ySize = this.getBoard().getVerticalSize();
+        int yDirection = (this.getTeam() == Team.WHITE) ? 1 : -1;
+        int startPos = (this.getTeam() == Team.WHITE) ? 1 : 6;
 
-        Square squareInFront = this.getBoard().getSquareByPos(xPos + 1, yPos);
+        Square squareInFront = this.getBoard().getSquareByPos(xPos, yPos + yDirection);
         if (squareInFront.getPiece() == null) {
             this.addMovableSquare(squareInFront);
         }
 
         if (xPos + 1 < xSize) {
-            Square squareInFrontRight = this.getBoard().getSquareByPos(xPos + 1, yPos + 1);
-            if (squareInFrontRight.getPiece() == null &&
-                    squareInFrontRight.getPiece().getTeam() != this.getTeam()) {
+            Square squareInFrontRight = this.getBoard().getSquareByPos(xPos + 1,
+                    yPos + yDirection);
+            if (squareInFrontRight.getPiece().getTeam() != this.getTeam()) {
                 this.addMovableSquare(squareInFrontRight);
             }
         }
 
         if (xPos > 0) {
-            Square squareInFrontLeft = this.getBoard().getSquareByPos(xPos + 1, yPos - 1);
-            if (squareInFrontLeft.getPiece() == null &&
-                    squareInFrontLeft.getPiece().getTeam() != this.getTeam()) {
+            Square squareInFrontLeft = this.getBoard().getSquareByPos(xPos - 1,
+                    yPos + yDirection);
+            if (squareInFrontLeft.getPiece().getTeam() != this.getTeam()) {
                 this.addMovableSquare(squareInFrontLeft);
             }
         }
 
 
-        if (yPos == 1){
-            Square squareTwoInFront = this.getBoard().getSquareByPos(xPos + 2, yPos);
+        if (yPos == startPos){
+            Square squareTwoInFront = this.getBoard().getSquareByPos(xPos, yPos + 2 * yDirection);
             if (squareTwoInFront.getPiece() == null) {
                 this.addMovableSquare(squareTwoInFront);
             }
