e9110edf39b00107c7f6dfacdc16eb50d0b1de56
elvis <elvissomers98@gmail.com>
2023-05-12 14:30:15 +0200

    Update player controller constructor and movepiece function

--- src/classes/controllers/Player.java
+++ src/classes/controllers/Player.java
@@ -1,16 +1,26 @@
 package classes.controllers;
 
+import classes.board.Square;
 import classes.pieces.Piece;
 
 import java.util.List;
 
 public class Player {
 
-    private enum Team {
+    public enum Team {
         WHITE, BLACK
     }
 
     private Team team;
 
+    // TODO: create specific pieceList class
     private List<Piece> pieces;
+
+    public Player(Team team) {
+        this.team = team;
+    }
+
+    public void movePiece(Piece piece, Square destSquare) {
+        piece.setSquare(destSquare);
+    }
 }
