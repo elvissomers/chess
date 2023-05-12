509f85db004e0b3ca70166526d91ae92b20a429d
elvis <elvissomers98@gmail.com>
2023-05-12 12:21:06 +0200

    setup queen class

---
+++ src/classes/pieces/Queen.java
@@ -0,0 +1,62 @@
+package classes.pieces;
+
+import classes.board.Square;
+
+public class Queen extends Piece{
+
+    @Override
+    public void setMoveableSquares() {
+        int xPos = this.getSquare().getHorizontalPosition();
+        int yPos = this.getSquare().getVerticalPosition();
+        int xSize = this.getBoard().getHorizontalSize();
+        int ySize = this.getBoard().getVerticalSize();
+
+        for (int direction = -1; direction <= 1; direction += 2) {
+            for (int x = xPos + direction; (direction > 0 ? x < xSize : x > 0); x += direction) {
+                Square currentSquare = this.getBoard().getSquareByPos(x, yPos);
+                if (currentSquare.getPiece() != null) {
+                    // TODO: & is not king
+                    if (currentSquare.getPiece().getTeam() != this.getTeam()) {
+                        this.addMovableSquare(currentSquare);
+                    }
+                    break;
+                }
+                this.addMovableSquare(currentSquare);
+            }
+        }
+
+        for (int direction = -1; direction <= 1; direction += 2) {
+            for (int y = yPos + direction; (direction > 0 ? y < ySize : y > 0); y += direction) {
+                Square currentSquare = this.getBoard().getSquareByPos(xPos, y);
+                if (currentSquare.getPiece() != null) {
+                    // TODO: & is not king
+                    if (currentSquare.getPiece().getTeam() != this.getTeam()) {
+                        this.addMovableSquare(currentSquare);
+                    }
+                    break;
+                }
+                this.addMovableSquare(currentSquare);
+            }
+        }
+
+        for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
+            for (int yRelativeDirection = -1; yRelativeDirection <= 1; yRelativeDirection += 2) {
+                for (int x = xPos + xDirection; (xDirection > 0 ? x < xSize : x > 0);
+                     x += xDirection) {
+                    for (int y = yPos + yRelativeDirection * xDirection;
+                         (xDirection > 0 ? y < ySize : y > 0); y += yRelativeDirection * xDirection) {
+                        Square currentSquare = this.getBoard().getSquareByPos(x, yPos);
+                        if (currentSquare.getPiece() != null) {
+                            // TODO: & is not king
+                            if (currentSquare.getPiece().getTeam() != this.getTeam()) {
+                                this.addMovableSquare(currentSquare);
+                            }
+                            break;
+                        }
+                        this.addMovableSquare(currentSquare);
+                    }
+                }
+            }
+        }
+    }
+}
