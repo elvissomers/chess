9b18eb7b3a07dff8ca588ec6e29a978d912d31d9
elvis <elvissomers98@gmail.com>
2023-05-12 11:05:31 +0200

    Abstract piece class

--- src/classes/pieces/Piece.java
+++ src/classes/pieces/Piece.java
@@ -1,6 +1,42 @@
 package classes.pieces;
 
-public class Piece {
+import classes.board.Board;
+import classes.board.Square;
 
-    // Just addding a comment to check if git is working
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Piece {
+
+    private Square square;
+
+    private Board board;
+
+    private enum team {
+        WHITE, BLACK
+    }
+
+    private List<Square> moveableSquares = new ArrayList<>();
+
+    public Square getSquare() {
+        return square;
+    }
+
+    public void setSquare(Square square) {
+        this.square = square;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public List<Square> getMoveableSquares() {
+        return moveableSquares;
+    }
+
+    public abstract void setMoveableSquares();
 }
