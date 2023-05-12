174c5b30b19474a3290656cd0684b82f2e85dddf
elvis <elvissomers98@gmail.com>
2023-05-12 12:46:50 +0200

    setup controller attributes

--- src/classes/controllers/Game.java
+++ src/classes/controllers/Game.java
@@ -1,4 +1,12 @@
 package classes.controllers;
 
+import classes.board.Board;
+
 public class Game {
+
+    private Board board;
+
+    private Player whitePlayer;
+
+    private Player blackPlayer;
 }
--- src/classes/controllers/Player.java
+++ src/classes/controllers/Player.java
@@ -1,4 +1,16 @@
 package classes.controllers;
 
+import classes.pieces.Piece;
+
+import java.util.List;
+
 public class Player {
+
+    private enum Team {
+        WHITE, BLACK
+    }
+
+    private Team team;
+
+    private List<Piece> pieces;
 }
