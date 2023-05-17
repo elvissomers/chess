323766a4f2844cb762c027942725cfc7ceefb0c2
elvis <elvissomers98@gmail.com>
2023-05-17 09:52:16 +0200

    bugfix: updated diagonal loop logic

--- src/main/classes/pieces/Bishop.java
+++ src/main/classes/pieces/Bishop.java
@@ -10,28 +10,49 @@ public class Bishop extends Piece {
 
     @Override
     public void setMovableSquares() {
+        System.out.println("hallooohoo?");
         int xPos = this.getSquare().getHorizontalPosition();
         int yPos = this.getSquare().getVerticalPosition();
         int xSize = this.getBoard().getHorizontalSize();
         int ySize = this.getBoard().getVerticalSize();
 
         for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
-            for (int yRelativeDirection = -1; yRelativeDirection <= 1; yRelativeDirection += 2) {
-                for (int x = xPos + xDirection; (xDirection > 0 ? x < xSize : x > 0);
-                     x += xDirection) {
-                    for (int y = yPos + yRelativeDirection * xDirection;
-                         (xDirection > 0 ? y < ySize : y > 0); y += yRelativeDirection * xDirection) {
-                        Square currentSquare = this.getBoard().getSquareByPos(x, yPos);
-                        if (currentSquare.getPiece() != null) {
-                            if (currentSquare.getPiece().getTeam() != this.getTeam()) {
-                                this.addMovableSquare(currentSquare);
-                            }
-                            break;
+            for (int yDirection = -1; yDirection <= 1; yDirection += 2) {
+                int x = xPos + xDirection;
+                int y = yPos + yDirection;
+
+                while ((x >= 0 && x < xSize) && (y >= 0 && y < ySize)) {
+                    Square currentSquare = this.getBoard().getSquareByPos(x, y);
+                    if (currentSquare.getPiece() != null) {
+                        if (currentSquare.getPiece().getTeam() != this.getTeam()) {
+                            this.addMovableSquare(currentSquare);
                         }
-                        this.addMovableSquare(currentSquare);
+                        break;
                     }
+                    this.addMovableSquare(currentSquare);
+                    x += xDirection;
+                    y += yDirection;
                 }
             }
         }
+
+//        for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
+//            for (int yRelativeDirection = -1; yRelativeDirection <= 1; yRelativeDirection += 2) {
+//                for (int x = xPos + xDirection; (xDirection > 0 ? x < xSize : x > 0);
+//                     x += xDirection) {
+//                    for (int y = yPos + yRelativeDirection * xDirection;
+//                         (xDirection * yRelativeDirection > 0 ? y < ySize : y > 0); y += yRelativeDirection * xDirection) {
+//                        Square currentSquare = this.getBoard().getSquareByPos(x, y);
+//                        if (currentSquare.getPiece() != null) {
+//                            if (currentSquare.getPiece().getTeam() != this.getTeam()) {
+//                                this.addMovableSquare(currentSquare);
+//                            }
+//                            break;
+//                        }
+//                        this.addMovableSquare(currentSquare);
+//                    }
+//                }
+//            }
+//        }
     }
 }
