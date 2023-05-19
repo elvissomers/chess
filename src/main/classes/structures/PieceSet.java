d3d6283d16ffdef8f821f0dee5eec6878cb48e08
elvis <elvissomers98@gmail.com>
2023-05-19 12:13:42 +0200

    update pieceset interface

--- src/main/classes/structures/PieceSet.java
+++ src/main/classes/structures/PieceSet.java
@@ -1,5 +1,6 @@
 package main.classes.structures;
 
+import main.classes.controllers.Player;
 import main.classes.pieces.King;
 import main.classes.pieces.Piece;
 
@@ -7,9 +8,13 @@ import java.util.Set;
 
 public interface PieceSet extends Set<Piece> {
 
-    King king = new King(null, null);
+    Player player = null;
 
     public void setAllAttackedSquares();
 
     public void setAllMovableSquares();
+
+    public King getKing();
+
+
 }
