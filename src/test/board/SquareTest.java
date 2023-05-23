132452f6535e38db62d9882a9c1393356245893c
elvis <elvissomers98@gmail.com>
2023-05-23 10:26:46 +0200

    Added second test

--- src/test/board/SquareTest.java
+++ src/test/board/SquareTest.java
@@ -8,9 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquareTest {
 
     @Test
-    void testToString() {
+    void testToStringA1() {
         Square square  = new Square(0,0);
 
         assertEquals("a1", square.toString());
+    }
+
+    @Test
+    void testToStringB6() {
+        Square square  = new Square(1,5);
+
+        assertEquals("b6", square.toString());
     }
 }
