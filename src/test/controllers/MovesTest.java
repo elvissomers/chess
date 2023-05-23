0ecfeb953c47febb9e92c97936ea42c882fa13c1
elvis <elvissomers98@gmail.com>
2023-05-23 10:42:28 +0200

    e4 can move to e5 test

--- src/test/controllers/MovesTest.java
+++ src/test/controllers/MovesTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import main.classes.structures.Team;
 
+import java.util.List;
+
 public class MovesTest {
     private Game game;
 
@@ -30,5 +32,12 @@ public class MovesTest {
     void testMoveStringPlayer(){
         Move e2e4 = game.getWhitePlayer().getMoveHistory().get(0);
         assertEquals("e2-e4",e2e4.toString());
+    }
+    @Test
+    void testPawnMovableSquares(){
+        game.getBoard().getSquareByPos(4,3).getPiece().setMovableSquares();
+        List<Square> e4Movables = game.getBoard().getSquareByPos(4,3).getPiece().getMovableSquares();
+        assertEquals(1, e4Movables.size());
+        assertEquals("e5",e4Movables.get(0).toString());
     }
 }
