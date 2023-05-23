4ddabdad1d85c216fdaff1e56b05e23ea3df1177
elvis <elvissomers98@gmail.com>
2023-05-23 10:39:02 +0200

    move toString testing

--- src/test/controllers/MovesTest.java
+++ src/test/controllers/MovesTest.java
@@ -2,6 +2,7 @@ package test.controllers;
 
 import main.classes.board.Board;
 import main.classes.board.Square;
+import main.classes.game.Move;
 import main.classes.pieces.*;
 import main.classes.controllers.Game;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +11,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import main.classes.structures.Team;
 
 public class MovesTest {
-    private Game Game;
+    private Game game;
 
     @BeforeEach
     public void setUp() {
-        Game = new Game();
+        game = new Game();
+        game.getWhitePlayer().movePiece(game.getBoard().getSquareByPos(4,1).getPiece(),
+                game.getBoard().getSquareByPos(4,3));
+    }
+
+    @Test
+    void testMoveStringGame(){
+        Move e2e4 = game.getMoveHistory().get(0);
+        assertEquals("e2-e4",e2e4.toString());
+    }
+
+    @Test
+    void testMoveStringPlayer(){
+        Move e2e4 = game.getWhitePlayer().getMoveHistory().get(0);
+        assertEquals("e2-e4",e2e4.toString());
     }
 }
