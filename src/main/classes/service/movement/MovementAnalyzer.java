1f8a955c02e00fb03e06775780d52f0416c2578e
elvis <elvissomers98@gmail.com>
2023-05-25 10:49:57 +0200

    Setup getLShapedMovableSquares to MovementAnalyzer.java

--- src/main/classes/service/movement/MovementAnalyzer.java
+++ src/main/classes/service/movement/MovementAnalyzer.java
@@ -85,23 +85,23 @@ public class MovementAnalyzer {
 
         for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
             for (int yRelativeDirection = -1; yRelativeDirection <= 1; yRelativeDirection += 2) {
-                int x1 = xPos + xDirection;
-                int x2 = xPos + 2 * xDirection;
-                int y1 = yPos + 2 * xDirection * yRelativeDirection;
-                int y2 = yPos + xDirection * yRelativeDirection;
+                int x1 = piece.getSquare().getHorizontalPosition() + xDirection;
+                int x2 = piece.getSquare().getHorizontalPosition() + 2 * xDirection;
+                int y1 = piece.getSquare().getVerticalPosition() + 2 * xDirection * yRelativeDirection;
+                int y2 = piece.getSquare().getVerticalPosition() + xDirection * yRelativeDirection;
 
-                if (0 <= x1 && x1 < xSize && 0 <= y1 && y1 < ySize){
-                    Square currentSquare = this.getBoard().getSquareByPos(x1, y1);
+                if (0 <= x1 && x1 < board.getHorizontalSize() && 0 <= y1 && y1 < board.getVerticalSize()){
+                    Square currentSquare = board.getSquareByPos(x1, y1);
                     if (currentSquare.getPiece() == null ||
-                            currentSquare.getPiece().getTeam() != this.getTeam()) {
-                        this.addMovableSquare(currentSquare);
+                            currentSquare.getPiece().getTeam() != piece.getTeam()) {
+                        movableSquares.add(currentSquare);
                     }
                 }
-                if (0 <= x2 && x2 < xSize && 0 <= y2 && y2 < ySize){
-                    Square currentSquare = this.getBoard().getSquareByPos(x2, y2);
+                if (0 <= x2 && x2 < board.getHorizontalSize() && 0 <= y2 && y2 < board.getVerticalSize()){
+                    Square currentSquare = board.getSquareByPos(x2, y2);
                     if (currentSquare.getPiece() == null ||
-                            currentSquare.getPiece().getTeam() != this.getTeam()) {
-                        this.addMovableSquare(currentSquare);
+                            currentSquare.getPiece().getTeam() != piece.getTeam()) {
+                        movableSquares.add(currentSquare);
                     }
                 }
             }
