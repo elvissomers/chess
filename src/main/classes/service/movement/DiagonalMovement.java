123761b04c1112eb62822a1a3bbc7087fccb9dd7
elvis <elvissomers98@gmail.com>
2023-05-25 10:28:38 +0200

    Setup diagonal movement computation in DiagonalMovement.java

--- src/main/classes/service/movement/DiagonalMovement.java
+++ src/main/classes/service/movement/DiagonalMovement.java
@@ -1,10 +1,38 @@
 package main.classes.service.movement;
 
+import main.classes.board.Board;
 import main.classes.board.Square;
+import main.classes.structures.Team;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class DiagonalMovement {
 
-    public Set<Square> getDiagonalMovableSquares()
+    private Board board;
+
+    public Set<Square> getDiagonalMovableSquares(int xPos, int yPos, Team team){
+        Set<Square> movableSquares = new HashSet<>();
+
+        for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
+            for (int yDirection = -1; yDirection <= 1; yDirection += 2) {
+                int x = xPos + xDirection;
+                int y = yPos + yDirection;
+
+                while ((x >= 0 && x < board.getHorizontalSize()) && (y >= 0 && y < board.getVerticalSize())) {
+                    Square currentSquare = board.getSquareByPos(x, y);
+                    if (currentSquare.getPiece() != null) {
+                        if (currentSquare.getPiece().getTeam() != team) {
+                            movableSquares.add(currentSquare);
+                        }
+                        break;
+                    }
+                    movableSquares.add(currentSquare);
+                    x += xDirection;
+                    y += yDirection;
+                }
+            }
+        }
+        return movableSquares;
+    }
 }
