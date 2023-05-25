2f71076ed2ca0bd78be1ae8fe9d763884b2ae146
elvis <elvissomers98@gmail.com>
2023-05-25 10:47:41 +0200

    Setup getLShapedMovableSquares to MovementAnalyzer.java

--- src/main/classes/service/movement/MovementAnalyzer.java
+++ src/main/classes/service/movement/MovementAnalyzer.java
@@ -79,4 +79,34 @@ public class MovementAnalyzer {
 
         return movableSquares;
     }
+
+    public Set<Square> getLShapedMovableSquares(){
+        Set<Square> movableSquares = new HashSet<>();
+
+        for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
+            for (int yRelativeDirection = -1; yRelativeDirection <= 1; yRelativeDirection += 2) {
+                int x1 = xPos + xDirection;
+                int x2 = xPos + 2 * xDirection;
+                int y1 = yPos + 2 * xDirection * yRelativeDirection;
+                int y2 = yPos + xDirection * yRelativeDirection;
+
+                if (0 <= x1 && x1 < xSize && 0 <= y1 && y1 < ySize){
+                    Square currentSquare = this.getBoard().getSquareByPos(x1, y1);
+                    if (currentSquare.getPiece() == null ||
+                            currentSquare.getPiece().getTeam() != this.getTeam()) {
+                        this.addMovableSquare(currentSquare);
+                    }
+                }
+                if (0 <= x2 && x2 < xSize && 0 <= y2 && y2 < ySize){
+                    Square currentSquare = this.getBoard().getSquareByPos(x2, y2);
+                    if (currentSquare.getPiece() == null ||
+                            currentSquare.getPiece().getTeam() != this.getTeam()) {
+                        this.addMovableSquare(currentSquare);
+                    }
+                }
+            }
+        }
+
+        return movableSquares;
+    }
 }
