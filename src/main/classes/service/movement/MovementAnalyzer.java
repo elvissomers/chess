a15ba72b4a9e206422a2b7a7bfbe8b2750f6a35e
elvis <elvissomers98@gmail.com>
2023-05-25 10:35:20 +0200

    Update MovementAnalyzer.java to contain Piece

--- src/main/classes/service/movement/MovementAnalyzer.java
+++ src/main/classes/service/movement/MovementAnalyzer.java
@@ -2,6 +2,7 @@ package main.classes.service.movement;
 
 import main.classes.board.Board;
 import main.classes.board.Square;
+import main.classes.pieces.Piece;
 import main.classes.structures.Team;
 
 import java.util.HashSet;
@@ -13,18 +14,18 @@ public class MovementAnalyzer {
 
     private Piece piece;
 
-    public Set<Square> getDiagonalMovableSquares(int xPos, int yPos, Team team){
+    public Set<Square> getDiagonalMovableSquares(){
         Set<Square> movableSquares = new HashSet<>();
 
         for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
             for (int yDirection = -1; yDirection <= 1; yDirection += 2) {
-                int x = xPos + xDirection;
-                int y = yPos + yDirection;
+                int x = piece.getSquare().getHorizontalPosition() + xDirection;
+                int y = piece.getSquare().getVerticalPosition() + yDirection;
 
                 while ((x >= 0 && x < board.getHorizontalSize()) && (y >= 0 && y < board.getVerticalSize())) {
                     Square currentSquare = board.getSquareByPos(x, y);
                     if (currentSquare.getPiece() != null) {
-                        if (currentSquare.getPiece().getTeam() != team) {
+                        if (currentSquare.getPiece().getTeam() != piece.getTeam()) {
                             movableSquares.add(currentSquare);
                         }
                         break;
