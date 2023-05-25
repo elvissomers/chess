591959c782b3956e04644d2edfd71b9c1ce33f9a
elvissomers <elvissomers98@gmail.com>
2023-05-25 12:00:18 +0200

    remove Board attribute from PieceSet.java

--- src/main/classes/structures/PieceSet.java
+++ src/main/classes/structures/PieceSet.java
@@ -16,23 +16,11 @@ public class PieceSet extends HashSet<Piece> implements IPieceSet{
 
     private Set<Square> attackedSquares;
 
-    private Board board;
-
     public PieceSet(Player player) {
         this.player = player;
         this.attackedSquares = new HashSet<>();
         player.setPieceSet(this);
     }
-
-    public PieceSet(Player player, Board board){
-        this.player = player;
-        this.board = board;
-        this.attackedSquares = new HashSet<>();
-    }
-
-//    public PieceSet(PieceSet other){
-//        this.
-//    }
 
     @Override
     public void setAllMovableSquares(){
