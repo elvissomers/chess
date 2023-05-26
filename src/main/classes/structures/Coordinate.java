e8fe06e17c138cd3ea8e0d5516905d51c3342208
elvissomers <elvissomers98@gmail.com>
2023-05-26 15:19:43 +0200

    Coordinate class ; simple tuple of 2 ints

---
+++ src/main/classes/structures/Coordinate.java
@@ -0,0 +1,20 @@
+package main.classes.structures;
+
+public class Coordinate {
+
+    int xPos;
+
+    int yPos;
+
+    public Coordinate(int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    @Override
+    public String toString(){
+        char[] horizontalIndices = {'a','b','c','d','e','f','g','h'};
+        String verticalIndex = Integer.toString(yPos+1);
+        return horizontalIndices[xPos] + verticalIndex;
+    }
+}
