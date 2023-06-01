971e41c90af0f20db33d2d65a5d68d3b7c48b35e
elvissomers <elvissomers98@gmail.com>
2023-06-01 10:04:52 +0200

    Update enum methods to public

--- src/main/classes/structures/MovementType.java
+++ src/main/classes/structures/MovementType.java
@@ -6,42 +6,42 @@ import main.classes.pieces.Piece;
 public enum MovementType {
     HORIZONTAL {
         @Override
-        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+        public void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
             moveFinder.setHorizontalMoves(piece, board);
         }
     },
     VERTICAL {
         @Override
-        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+        public void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
             moveFinder.setVerticalMoves(piece, board);
         }
     },
     DIAGONAL {
         @Override
-        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+        public void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
             moveFinder.setDiagonalMoves(piece, board);
         }
     },
     LSHAPED {
         @Override
-        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+        public void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
             moveFinder.setLShapedMoves(piece, board);
         }
     },
     PAWN {
         @Override
-        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+        public void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
             moveFinder.setPawnMoves(piece, board);
         }
     },
     KING {
         @Override
-        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+        public void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
             moveFinder.setKingBasicMoves(piece, board);
             moveFinder.setKingCastlingMoves(piece, board);
         }
     };
 
-    abstract void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder);
+    public abstract void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder);
 }
 
