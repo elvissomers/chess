64c70f5dd1fe24802fdb33e777a7327b97ef2508
elvissomers <elvissomers98@gmail.com>
2023-06-01 10:02:29 +0200

    Update MovementType enum to contain functionality

--- src/main/classes/structures/MovementType.java
+++ src/main/classes/structures/MovementType.java
@@ -1,5 +1,47 @@
 package main.classes.structures;
 
+import main.classes.movement.MovementFinder;
+import main.classes.pieces.Piece;
+
 public enum MovementType {
-    HORIZONTAL, VERTICAL, DIAGONAL, LSHAPED, PAWN, KING
+    HORIZONTAL {
+        @Override
+        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+            moveFinder.setHorizontalMoves(piece, board);
+        }
+    },
+    VERTICAL {
+        @Override
+        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+            moveFinder.setVerticalMoves(piece, board);
+        }
+    },
+    DIAGONAL {
+        @Override
+        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+            moveFinder.setDiagonalMoves(piece, board);
+        }
+    },
+    LSHAPED {
+        @Override
+        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+            moveFinder.setLShapedMoves(piece, board);
+        }
+    },
+    PAWN {
+        @Override
+        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+            moveFinder.setPawnMoves(piece, board);
+        }
+    },
+    KING {
+        @Override
+        void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder) {
+            moveFinder.setKingBasicMoves(piece, board);
+            moveFinder.setKingCastlingMoves(piece, board);
+        }
+    };
+
+    abstract void setMoves(Piece piece, BoardMap board, MovementFinder moveFinder);
 }
+
