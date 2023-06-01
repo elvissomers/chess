bf28ff108566f46e4b2f338a8b86cfdec88ca3dc
elvissomers <elvissomers98@gmail.com>
2023-06-01 10:09:38 +0200

    un abstracted setMovableSquares -> implementation in Bishop

--- src/main/classes/pieces/Bishop.java
+++ src/main/classes/pieces/Bishop.java
@@ -3,7 +3,10 @@ package main.classes.pieces;
 import main.classes.board.Square;
 import main.classes.controllers.Game;
 import main.classes.controllers.Player;
+import main.classes.structures.MovementType;
 import main.classes.structures.Team;
+
+import java.util.Set;
 
 public class Bishop extends Piece {
 
@@ -18,36 +21,7 @@ public class Bishop extends Piece {
 
     public Bishop(Player player){
         super(player);
-    }
-
-    @Override
-    public void setMovableSquares() {
-        removePreviousMovableSquares();
-
-        int xPos = this.getSquare().getHorizontalPosition();
-        int yPos = this.getSquare().getVerticalPosition();
-        int xSize = this.getBoard().getHorizontalSize();
-        int ySize = this.getBoard().getVerticalSize();
-
-        for (int xDirection = -1; xDirection <= 1; xDirection += 2) {
-            for (int yDirection = -1; yDirection <= 1; yDirection += 2) {
-                int x = xPos + xDirection;
-                int y = yPos + yDirection;
-
-                while ((x >= 0 && x < xSize) && (y >= 0 && y < ySize)) {
-                    Square currentSquare = this.getBoard().getSquareByPos(x, y);
-                    if (currentSquare.getPiece() != null) {
-                        if (currentSquare.getPiece().getTeam() != this.getTeam()) {
-                            this.addMovableSquare(currentSquare);
-                        }
-                        break;
-                    }
-                    this.addMovableSquare(currentSquare);
-                    x += xDirection;
-                    y += yDirection;
-                }
-            }
-        }
+        this.setMoveRules(Set.of(MovementType.DIAGONAL));
     }
 
     public Bishop copy(){
