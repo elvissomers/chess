1746242ee4614ed7bb0d2fb2fdcd79f73d62a396
elvissomers <elvissomers98@gmail.com>
2023-06-01 12:06:34 +0200

    remove some comments

--- src/main/classes/pieces/King.java
+++ src/main/classes/pieces/King.java
@@ -16,9 +16,6 @@ public class King extends Piece {
         super(other);
     }
 
-    // King keeps track of whether it has moved during this game
-    // - needed for castling rule
-    // TODO: will remove this, obtain it from the move history instead
     private boolean hasMoved;
 
     private boolean inCheck;
@@ -38,18 +35,6 @@ public class King extends Piece {
     public void setHasMoved(boolean hasMoved) {
         this.hasMoved = hasMoved;
     }
-
-
-    /**
-     * Separate checkCheck method that is used for checking a hypothetical board,
-     * that is, a board that does not belong to a Game and.
-     *
-     * It does not need a Square as input, as it only checks if the King is in check
-     * on the square that it currently is on.
-     */
-//    public boolean checkCheck(Board board){
-//        return false;
-//    }
 
     public King copy(){
         return new King(this);
