e2ae7744c376da2ee6c3b61515aa71d086031cfb
elvissomers <elvissomers98@gmail.com>
2023-06-02 09:04:07 +0200

    Put promotion into separate method

--- src/main/classes/movement/MoveMaker.java
+++ src/main/classes/movement/MoveMaker.java
@@ -10,55 +10,6 @@ import main.classes.structures.Team;
 
 public class MoveMaker {
 
-    // TODO: separate into more methods, so methods "do only one thing".
-    public void movePiece(Game game, Piece piece, Coordinate destination) {
-        Player thisPlayer = piece.getPlayer();
-        Player enemyPlayer = (thisPlayer.getTeam() == Team.WHITE) ? game.getBlackPlayer() : game.getWhitePlayer();
-
-        Coordinate fromSquare = piece.getPosition();
-        Move move = new Move(piece, fromSquare, destination);
-        piece.setPosition(destination);
-
-        // A piece is "taken" when an enemy piece moves to its square
-        Piece takenPiece = game.getBoard().get(destination);
-        if (takenPiece != null) {
-            enemyPlayer.getPieces().remove(takenPiece);
-        }
-
-        if (piece instanceof King king && !king.isHasMoved()){
-            if (destination.getX() - fromSquare.getX() == 2) {
-                castle(game, king, (Rook) game.getBoard().getPieceByPos(7,fromSquare.getY()),
-                        destination, CastleType.SHORT
-                );
-                return;
-            } else if (destination.getX() - fromSquare.getX() == -2) {
-                castle(game, king, (Rook) game.getBoard().getPieceByPos(0,fromSquare.getY()),
-                        destination, CastleType.LONG
-                );
-                return;
-            }
-            king.setHasMoved(true);
-        }
-
-        if (piece instanceof Rook rook && !rook.isHasMoved()){
-            rook.setHasMoved(true);
-        }
-
-        int promotionRank = (thisPlayer.getTeam() == Team.WHITE) ? 7 : 0;
-
-        if (piece instanceof Pawn pawn && destination.getY() == promotionRank){
-            thisPlayer.getPieces().remove(pawn);
-            // TODO: let player choose a piece instead of always having a queen
-            Queen promotionPiece = new Queen(thisPlayer);
-            game.getBoard().put(destination, promotionPiece);
-            thisPlayer.getPieces().add(promotionPiece);
-            // TODO: add "promoted" attribute to Move object, with the piece specification
-        } else {
-            game.getBoard().put(destination, piece);
-        }
-        thisPlayer.getMoveHistory().add(move);
-    }
-
     public void castle(Game game, King king, Rook rook, Coordinate destination, CastleType type){
         king.setHasMoved(true);
         rook.setHasMoved(true);
@@ -120,23 +71,26 @@ public class MoveMaker {
         }
 
         if (move.isPromoted()){
-            game.getBoard().remove(move.getPiece().getPosition());
-
-            Queen queen = new Queen(move.getPiece().getPlayer());
-            int yDirection = (move.getPiece().getPlayer().getTeam() == Team.WHITE) ? 1 : -1;
-            queen.setPosition(game.getBoard().getCoordinateByPos(move.getPiece().getPosition().getX(),
-                    move.getPiece().getPosition().getY()+yDirection));
-            game.getBoard().put(game.getBoard().getCoordinateByPos(move.getPiece().getPosition().getX(),
-                    move.getPiece().getPosition().getY()+yDirection), queen);
-
-            thisPlayer.getPieces().remove(move.getPiece());
-            thisPlayer.getPieces().add(queen);
+            promote(move.getPiece(), game, thisPlayer);
         } else {
             game.getBoard().put(move.getSquareFrom(), null);
             game.getBoard().put(move.getSquareTo(), move.getPiece());
             move.getPiece().setPosition(move.getSquareTo());
         }
+    }
 
+    public void promote(Piece piece, Game game, Player player){
+        game.getBoard().remove(piece.getPosition());
 
+        // TODO: player should be able to pick a piece, instead of always getting a queen!
+        Queen queen = new Queen(piece.getPlayer());
+        int yDirection = (piece.getPlayer().getTeam() == Team.WHITE) ? 1 : -1;
+        queen.setPosition(game.getBoard().getCoordinateByPos(piece.getPosition().getX(),
+                piece.getPosition().getY()+yDirection));
+        game.getBoard().put(game.getBoard().getCoordinateByPos(piece.getPosition().getX(),
+                piece.getPosition().getY()+yDirection), queen);
+
+        player.getPieces().remove(piece);
+        player.getPieces().add(queen);
     }
 }
