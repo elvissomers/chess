e4c4df7ad750cb4cd11313f273ad483006d35765
elvissomers <elvissomers98@gmail.com>
2023-06-05 15:37:08 +0200

    initialize Maven support

---
+++ src/main/classes/controllers/PlayGame.java
@@ -0,0 +1,12 @@
+package main.classes.controllers;
+
+import main.classes.instances.Game;
+import main.classes.structures.Team;
+
+public class PlayGame {
+
+    private Team playerTurn;
+
+    private Game game;
+
+}
