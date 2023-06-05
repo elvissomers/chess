4056c5634a98e0ed6bac0e0d447eb97ea13ae5db
elvissomers <elvissomers98@gmail.com>
2023-06-05 10:07:36 +0200

    Added "legalMovableSquares" attribute aside from just movableSquares

--- src/main/classes/pieces/Piece.java
+++ src/main/classes/pieces/Piece.java
@@ -60,6 +60,7 @@ public abstract class Piece {
          * before setting new ones.
          */
         movableSquares = new ArrayList<>();
+        legalMovableSquares = new ArrayList<>();
     }
 
     public void setMovableSquares(){
