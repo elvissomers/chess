b71a22434b8643727e30ba5d2e63d099a515b4f8
elvissomers <elvissomers98@gmail.com>
2023-06-08 17:05:51 +0200

    Setup get method in PlayGame.java (will be setup as Spring controller later)

--- src/main/java/classes/controllers/PlayGame.java
+++ src/main/java/classes/controllers/PlayGame.java
@@ -1,12 +1,20 @@
 package classes.controllers;
 
 import classes.instances.Game;
+import classes.structures.Coordinate;
 import classes.structures.Team;
+
+import java.util.List;
 
 public class PlayGame {
 
     private Team playerTurn;
 
     private Game game;
+
+    @GetRequest
+    private List<Coordinate> getMovableCoordinatesForPiece(int i, int j){
+        return game.getBoard().getPieceByPos(i,j).getMovableSquares();
+    }
 
 }
