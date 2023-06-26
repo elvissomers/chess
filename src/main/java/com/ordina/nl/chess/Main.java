4aa2163b4ca3eef3018d60b1e03eb384d88c956a
elvissomers <elvissomers98@gmail.com>
2023-06-26 14:42:35 +0200

    comment out unused test

--- src/main/java/com/ordina/nl/chess/Main.java
+++ src/main/java/com/ordina/nl/chess/Main.java
@@ -8,27 +8,27 @@ import com.ordina.nl.chess.structures.Team;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.update(Team.WHITE);
-        System.out.println(game.getBoard());
-
-        Move e2e4 = game.getMoveMaker().getMove(game, game.getBoard().getPieceByPos(4,1),
-                game.getBoard().getCoordinateByPos(4,3));
-        game.getMoveMaker().makeMove(e2e4, game);
-        game.update(Team.BLACK);
-        System.out.println(game.getBoard());
-
-        Move f7f5 = game.getMoveMaker().getMove(game, game.getBoard().getPieceByPos(5,6),
-                game.getBoard().getCoordinateByPos(5,4));
-        game.getMoveMaker().makeMove(f7f5, game);
-        game.update(Team.WHITE);
-        System.out.println(game.getBoard());
-
-        Move Qh5 = game.getMoveMaker().getMove(game, game.getBoard().getPieceByPos(3,0),
-                game.getBoard().getCoordinateByPos(7,4));
-        game.getMoveMaker().makeMove(Qh5, game);
-        game.update(Team.BLACK);
-        System.out.println(game.getBoard());
+//        Game game = new Game();
+//        game.update(Team.WHITE);
+//        System.out.println(game.getBoard());
+//
+//        Move e2e4 = game.getMoveMaker().getMove(game, game.getBoard().getPieceByPos(4,1),
+//                game.getBoard().getCoordinateByPos(4,3));
+//        game.getMoveMaker().makeMove(e2e4, game);
+//        game.update(Team.BLACK);
+//        System.out.println(game.getBoard());
+//
+//        Move f7f5 = game.getMoveMaker().getMove(game, game.getBoard().getPieceByPos(5,6),
+//                game.getBoard().getCoordinateByPos(5,4));
+//        game.getMoveMaker().makeMove(f7f5, game);
+//        game.update(Team.WHITE);
+//        System.out.println(game.getBoard());
+//
+//        Move Qh5 = game.getMoveMaker().getMove(game, game.getBoard().getPieceByPos(3,0),
+//                game.getBoard().getCoordinateByPos(7,4));
+//        game.getMoveMaker().makeMove(Qh5, game);
+//        game.update(Team.BLACK);
+//        System.out.println(game.getBoard());
 
     }
 }
