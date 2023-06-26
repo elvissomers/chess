0e44f9f27f5ec1e45f86e1e7d4b4c1d008239966
elvissomers <elvissomers98@gmail.com>
2023-06-26 13:39:45 +0200

    update inCheck to not be an attribute

--- src/main/java/com/ordina/nl/chess/pieces/King.java
+++ src/main/java/com/ordina/nl/chess/pieces/King.java
@@ -17,8 +17,6 @@ public class King extends Piece {
     // TODO: have optional fields present in Piece (but null in pieces that don't have it)?
     private boolean hasMoved;
 
-    private boolean inCheck;
-
     public King(Player player){
         super(player);
         this.setMoveRules(Set.of(MovementType.KING));
@@ -30,10 +28,6 @@ public class King extends Piece {
 
     public King copy(){
         return new King(this);
-    }
-
-    public boolean isInCheck() {
-        return inCheck;
     }
 
     public boolean isHasMoved() {
@@ -55,11 +49,12 @@ public class King extends Piece {
     }
 
     // TODO: this might be redundant? The whole "check" attribute?
-    public void setInCheck(){
+    public boolean isInCheck(){
         Player attackingPlayer = (getPlayer().getTeam() == Team.WHITE) ? getPlayer().getGame().getBlackPlayer() :
                 getPlayer().getGame().getWhitePlayer();
         Set<Coordinate> allAttackedPositions = attackingPlayer.getAllAttackedSquares();
-        inCheck = allAttackedPositions.contains(getPosition());
+        Coordinate kingCoordinate = new Coordinate(getHorizontalPosition(), getVerticalPosition());
+        return allAttackedPositions.contains(kingCoordinate);
     }
 
 }
