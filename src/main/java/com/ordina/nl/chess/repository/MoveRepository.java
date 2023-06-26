7fd6cedc22f88eae7424ea356b95f537df2d118c
elvissomers <elvissomers98@gmail.com>
2023-06-26 13:14:26 +0200

    added super long query

--- src/main/java/com/ordina/nl/chess/repository/MoveRepository.java
+++ src/main/java/com/ordina/nl/chess/repository/MoveRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MoveRepository extends JpaRepository<Move, Long> {
 
     List<Move> findByPlayers_IdContaining(long id);
 
+    Optional<Move> findByNumberAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalTo(int number, int xFrom,
+                                                                                            int xTo, int yFrom, int yTo);
 
 }
