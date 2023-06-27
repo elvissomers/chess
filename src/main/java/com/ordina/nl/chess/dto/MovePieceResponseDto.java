476ac3d819f0949bbd2f8424b6d1f7d0cc304572
elvissomers <elvissomers98@gmail.com>
2023-06-27 09:54:10 +0200

    added dto

---
+++ src/main/java/com/ordina/nl/chess/dto/MovePieceResponseDto.java
@@ -0,0 +1,10 @@
+package com.ordina.nl.chess.dto;
+
+public class MovePieceResponseDto {
+
+    private String message;
+
+    public MovePieceResponseDto(String message) {
+        this.message = message;
+    }
+}
