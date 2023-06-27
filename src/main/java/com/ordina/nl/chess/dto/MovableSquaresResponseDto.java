d36346aee910eeda1f70d8e157de06644d8b1d72
elvissomers <elvissomers98@gmail.com>
2023-06-27 09:53:29 +0200

    update dto

---
+++ src/main/java/com/ordina/nl/chess/dto/MovableSquaresResponseDto.java
@@ -0,0 +1,24 @@
+package com.ordina.nl.chess.dto;
+
+import com.ordina.nl.chess.structures.Coordinate;
+
+import java.util.List;
+
+public class MovableSquaresResponseDto {
+
+    private String message;
+
+    private List<Coordinate> movableSquares;
+
+    public MovableSquaresResponseDto(String message) {
+        this.message = message;
+    }
+
+    public List<Coordinate> getMovableSquares() {
+        return movableSquares;
+    }
+
+    public void setMovableSquares(List<Coordinate> movableSquares) {
+        this.movableSquares = movableSquares;
+    }
+}
