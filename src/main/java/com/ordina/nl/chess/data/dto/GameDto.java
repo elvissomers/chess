57c3a267d5253628093148a621c79d0b17589b7a
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-18 15:23:48 +0200

    Setup gameDto & converter

--- src/main/java/com/ordina/nl/chess/data/dto/GameDto.java
+++ src/main/java/com/ordina/nl/chess/data/dto/GameDto.java
@@ -1,4 +1,22 @@
 package com.ordina.nl.chess.data.dto;
 
+import com.ordina.nl.chess.enums.GameState;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GameDto {
+
+    private long id;
+    private long whitePlayerId;
+    private long blackPlayerId;
+
+    @Enumerated(EnumType.STRING)
+    private GameState gameState;
+
 }
--- src/main/java/com/ordina/nl/chess/service/GameService.java
+++ src/main/java/com/ordina/nl/chess/service/GameService.java
@@ -1,4 +1,25 @@
 package com.ordina.nl.chess.service;
 
+import com.ordina.nl.chess.data.dto.GameDto;
+import com.ordina.nl.chess.data.dto.mapper.GameDtoMapper;
+import com.ordina.nl.chess.exception.ElementNotFoundException;
+import com.ordina.nl.chess.repository.GameRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class GameService {
+
+    private final GameRepository gameRepository;
+
+    private final GameDtoMapper gameDtoMapper;
+
+    public GameDto getGame(long id) throws ElementNotFoundException {
+        return gameRepository.findById(id)
+                .map(gameDtoMapper::gameToGameDto)
+                .orElse(null);
+        // TODO; when BaseExceptionHandler is put up:
+//                .orElseThrow(() -> new ElementNotFoundException("Requested game ID does not correspond to an existing game!"));
+    }
 }
