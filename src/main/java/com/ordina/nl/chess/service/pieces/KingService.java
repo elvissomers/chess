e399aa467de6f7028b2d2d98ce5dbd4d731b8baf
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-18 14:59:44 +0200

    Setup Service classes for pieces of all types

---
+++ src/main/java/com/ordina/nl/chess/service/pieces/KingService.java
@@ -0,0 +1,4 @@
+package com.ordina.nl.chess.service.pieces;
+
+public class KingService {
+}
--- src/main/java/com/ordina/nl/chess/service/pieces/PieceService.java
+++ src/main/java/com/ordina/nl/chess/service/pieces/PieceService.java
@@ -1,16 +1,22 @@
-package com.ordina.nl.chess.service;
+package com.ordina.nl.chess.service.pieces;
 
 import com.ordina.nl.chess.entity.pieces.Piece;
 import com.ordina.nl.chess.enums.MovementType;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class PieceService {
+
+    private final PawnService pawnService;
+    private final KnightService knightService;
+    private final BishopService bishopService;
+    private final RookService rookService;
+    private final QueenService queenService;
+    private final KingService kingService;
 
     public void setMovementTypesForPiece(Piece piece) {
         List<MovementType> movementTypes = piece.getMovementTypes();
