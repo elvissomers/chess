2814c638165ae4c6cb067197558caf8c393f1687
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-18 16:33:34 +0200

    add find player method in PlayerService.java

--- src/main/java/com/ordina/nl/chess/service/PlayerService.java
+++ src/main/java/com/ordina/nl/chess/service/PlayerService.java
@@ -1,9 +1,13 @@
 package com.ordina.nl.chess.service;
 
+import com.ordina.nl.chess.data.dto.GameDto;
+import com.ordina.nl.chess.data.dto.PlayerDto;
+import com.ordina.nl.chess.data.dto.mapper.PlayerDtoMapper;
 import com.ordina.nl.chess.entity.Player;
 import com.ordina.nl.chess.entity.pieces.*;
 import com.ordina.nl.chess.enums.PieceType;
 import com.ordina.nl.chess.enums.Team;
+import com.ordina.nl.chess.repository.PlayerRepository;
 import com.ordina.nl.chess.service.pieces.PieceService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,18 @@ import static com.ordina.nl.chess.constants.BoardSize.horizontalSize;
 public class PlayerService {
 
     private PieceService pieceService;
+
+    private PlayerRepository playerRepository;
+
+    private PlayerDtoMapper playerDtoMapper;
+
+    public PlayerDto getPlayer(long id) {
+        return playerRepository.findById(id)
+                .map(playerDtoMapper::playerToPlayerDto)
+                .orElse(null);
+        // TODO; when BaseExceptionHandler is put up:
+//                .orElseThrow(() -> new ElementNotFoundException("!"));
+    }
 
     public void setStartPiecesForPlayer(Player player) {
         PieceType[] pieceTypesInOrder = new PieceType[]{
