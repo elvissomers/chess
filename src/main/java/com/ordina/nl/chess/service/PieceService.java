dd6584d6e7bc0d394a370aa7922323c444fb71af
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-18 14:57:00 +0200

    Add setMovementTypeForPiece method to PieceService.java

--- src/main/java/com/ordina/nl/chess/service/PieceService.java
+++ src/main/java/com/ordina/nl/chess/service/PieceService.java
@@ -1,4 +1,33 @@
 package com.ordina.nl.chess.service;
 
+import com.ordina.nl.chess.entity.pieces.Piece;
+import com.ordina.nl.chess.enums.MovementType;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
 public class PieceService {
+
+    public void setMovementTypesForPiece(Piece piece) {
+        List<MovementType> movementTypes = piece.getMovementTypes();
+        switch(piece.getPieceType()) {
+            case KNIGHT -> movementTypes.add(MovementType.LSHAPED);
+            case BISHOP -> movementTypes.add(MovementType.DIAGONAL);
+            case KING -> movementTypes.add(MovementType.KING);
+            case PAWN -> movementTypes.add(MovementType.PAWN);
+            case ROOK -> {
+                movementTypes.add(MovementType.HORIZONTAL);
+                movementTypes.add(MovementType.VERTICAL);
+            }
+            case QUEEN -> {
+                movementTypes.add(MovementType.DIAGONAL);
+                movementTypes.add(MovementType.HORIZONTAL);
+                movementTypes.add(MovementType.VERTICAL);
+            }
+        }
+    }
 }
