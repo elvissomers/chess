85bf89bf8ac203d2378f00b9da1e2898db7a3149
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-19 13:12:31 +0200

    Repository search in MoveService.java

--- src/main/java/com/ordina/nl/chess/service/MoveService.java
+++ src/main/java/com/ordina/nl/chess/service/MoveService.java
@@ -1,4 +1,30 @@
 package com.ordina.nl.chess.service;
 
+import com.ordina.nl.chess.data.dto.MoveDto;
+import com.ordina.nl.chess.data.dto.mapper.MoveDtoMapper;
+import com.ordina.nl.chess.entity.Move;
+import com.ordina.nl.chess.repository.MoveRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class MoveService {
+
+    private final MoveRepository moveRepository;
+
+    private final MoveDtoMapper moveDtoMapper;
+
+    public Move getMove(long id) {
+        return moveRepository.findById(id)
+                .orElse(null);
+        // TODO .orElseThrow(ElementNotFoundException)
+    }
+
+    public MoveDto getMoveDto(long id) {
+        return moveRepository.findById(id)
+                .map(moveDtoMapper::MoveToMoveDto)
+                .orElse(null);
+        // TODO .orElseThrow(ElementNotFoundException)
+    }
 }
