32c89712a147c464914a2f0e2fcd5942ff44a9ff
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-07-20 11:43:51 +0200

    setup BishopService.java

--- src/main/java/com/ordina/nl/chess/service/pieces/BishopService.java
+++ src/main/java/com/ordina/nl/chess/service/pieces/BishopService.java
@@ -1,4 +1,24 @@
 package com.ordina.nl.chess.service.pieces;
 
+import com.ordina.nl.chess.entity.pieces.Piece;
+import com.ordina.nl.chess.service.BoardService;
+import com.ordina.nl.chess.service.MoveOptionService;
+import com.ordina.nl.chess.service.structures.BoardMap;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class BishopService {
+
+    private final BoardService boardService;
+    private final MoveOptionService moveOptionService;
+
+    private int xPos;
+    private int yPos;
+
+    public void setMovableSquares(Piece piece, long gameId) {
+        BoardMap board = boardService.getBoardMapForGame(gameId);
+        moveOptionService.setDiagonalMoves(piece, board);
+    }
 }
