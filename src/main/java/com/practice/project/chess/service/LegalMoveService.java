866f0d826c830d70ea0d007fb53ef2c737150bef
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-08-30 12:28:47 +0200

    setup LegalMoveService.java

---
+++ src/main/java/com/practice/project/chess/service/LegalMoveService.java
@@ -0,0 +1,11 @@
+package com.practice.project.chess.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class LegalMoveService {
+
+
+}
