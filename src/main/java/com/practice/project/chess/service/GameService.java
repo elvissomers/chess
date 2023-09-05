d73e6110698fd74a2055982f306b9f42570e7400
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-05 10:23:34 +0200

    Added getPlayerForGameAndTeam and updated exception throwing

--- src/main/java/com/practice/project/chess/service/GameService.java
+++ src/main/java/com/practice/project/chess/service/GameService.java
@@ -3,6 +3,9 @@ package com.practice.project.chess.service;
 import com.practice.project.chess.data.dto.GameDto;
 import com.practice.project.chess.data.dto.mapper.GameDtoMapper;
 import com.practice.project.chess.entity.Game;
+import com.practice.project.chess.entity.Player;
+import com.practice.project.chess.enums.Team;
+import com.practice.project.chess.exception.ElementNotFoundException;
 import com.practice.project.chess.repository.GameRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,18 +18,23 @@ public class GameService {
 
     private final GameDtoMapper gameDtoMapper;
 
-    public Game getGame(long id) {
+    public Game getGame(long id) throws ElementNotFoundException {
         return gameRepository.findById(id)
-                .orElse(null);
-        // TODO; when BaseExceptionHandler is put up:
-//                .orElseThrow(() -> new ElementNotFoundException("Requested game ID does not correspond to an existing game!"));;
+                .orElseThrow(() -> new ElementNotFoundException(
+                        "Requested game ID does not correspond to an existing game!"));;
     }
 
-    public GameDto getGameDto(long id) {
+    public GameDto getGameDto(long id) throws ElementNotFoundException {
         return gameRepository.findById(id)
                 .map(gameDtoMapper::gameToGameDto)
-                .orElse(null);
-        // TODO; when BaseExceptionHandler is put up:
-//                .orElseThrow(() -> new ElementNotFoundException("Requested game ID does not correspond to an existing game!"));
+                .orElseThrow(() -> new ElementNotFoundException(
+                        "Requested game ID does not correspond to an existing game!"));
+    }
+
+    public Player getPlayerForGameAndTeam(long gameId, Team team) throws ElementNotFoundException {
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new ElementNotFoundException(
+                        "Requested game ID does not correspond to an existing game!"));
+        return (team == Team.WHITE) ? game.getWhitePlayer() : game.getBlackPlayer();
     }
 }
