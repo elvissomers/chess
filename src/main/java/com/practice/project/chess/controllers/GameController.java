ebce926dbce2166d70488d1e03a50d9da12b5f11
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-07 13:20:25 +0200

    GameController.java update (getGame)

--- src/main/java/com/practice/project/chess/controllers/GameController.java
+++ src/main/java/com/practice/project/chess/controllers/GameController.java
@@ -2,35 +2,30 @@ package com.practice.project.chess.controllers;
 
 import com.practice.project.chess.data.dto.GameDto;
 import com.practice.project.chess.entity.Game;
+import com.practice.project.chess.exception.ElementNotFoundException;
 import com.practice.project.chess.repository.GameRepository;
+import com.practice.project.chess.service.GameService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("game")
 @CrossOrigin(maxAge = 3600)
 public class GameController {
 
-    @Autowired
-    private GameRepository gameRepository;
+    private final GameService gameService;
 
     @GetMapping("view/{id}")
-    public GameDto getGame(@PathVariable long id) {
-        // TODO: setup GameDto!
-        Optional<Game> optionalGame = gameRepository.findById(id);
-        if (optionalGame.isEmpty()) {
-            return null;
-        }
-        // TODO: GameDto = optionalGame.get().toDto();
-        return null;
+    public GameDto getGame(@PathVariable long id) throws ElementNotFoundException {
+        return gameService.getGameDto(id);
     }
 
     @PutMapping("create_new")
     public void createNewStandardGame() {
-        Game game = new Game();
-        game.setStandardStartingGame();
-        gameRepository.save(game);
+        // TODO
     }
 }
