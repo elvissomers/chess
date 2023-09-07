75c9f3fe0b4213916485ba62fb21fa317b1bdc12
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-07 13:35:12 +0200

    getNewGame method in GameService.java

--- src/main/java/com/practice/project/chess/service/GameService.java
+++ src/main/java/com/practice/project/chess/service/GameService.java
@@ -4,6 +4,7 @@ import com.practice.project.chess.data.dto.GameDto;
 import com.practice.project.chess.data.dto.mapper.GameDtoMapper;
 import com.practice.project.chess.entity.Game;
 import com.practice.project.chess.entity.Player;
+import com.practice.project.chess.enums.GameState;
 import com.practice.project.chess.enums.Team;
 import com.practice.project.chess.exception.ElementNotFoundException;
 import com.practice.project.chess.exception.InvalidMoveException;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class GameService {
 
     private final PieceService pieceService;
+    private final PlayerService playerService;
 
     private final GameRepository gameRepository;
 
@@ -34,6 +36,20 @@ public class GameService {
                 .map(gameDtoMapper::gameToGameDto)
                 .orElseThrow(() -> new ElementNotFoundException(
                         "Requested game ID does not correspond to an existing game!"));
+    }
+
+    public GameDto getNewGame() {
+        Game game = new Game();
+        Player whitePlayer = Player.builder().game(game).team(Team.WHITE).build();
+        Player blackPlayer = Player.builder().game(game).team(Team.BLACK).build();
+
+        playerService.setStartPiecesForPlayer(whitePlayer);
+        playerService.setStartPiecesForPlayer(blackPlayer);
+
+        game.setWhitePlayer(whitePlayer);
+        game.setBlackPlayer(blackPlayer);
+        game.setGameState(GameState.WHITE_TURN);
+        return gameDtoMapper.gameToGameDto(gameRepository.save(game));
     }
 
     public Player getPlayerForGameAndTeam(long gameId, Team team) throws ElementNotFoundException {
