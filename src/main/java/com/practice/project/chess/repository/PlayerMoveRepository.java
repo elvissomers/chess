b062e13b5b1f09e1cd3aba05dd5a4e6f184cc344
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-18 12:31:41 +0200

    Derived queries

--- src/main/java/com/practice/project/chess/repository/PlayerMoveRepository.java
+++ src/main/java/com/practice/project/chess/repository/PlayerMoveRepository.java
@@ -5,10 +5,15 @@ import com.practice.project.chess.entity.PlayerMove;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PlayerMoveRepository extends JpaRepository<PlayerMove, Long> {
 
     Optional<PlayerMove> findByNumberAndMove(int number, Move move);
+
+    Optional<PlayerMove> findByNumberAndPlayer_Id(int number, long playerId);
+
+    List<PlayerMove> findByPlayer_Id(long playerId);
 }
