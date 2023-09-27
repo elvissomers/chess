c82158154177c22bb3a64a9b40de3077ebdfa162
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-27 12:01:30 +0200

    Removed unused enum MovementType.java

--- src/main/java/com/practice/project/chess/enums/PieceType.java
+++ src/main/java/com/practice/project/chess/enums/PieceType.java
@@ -1,46 +1,10 @@
 package com.practice.project.chess.enums;
 
-import java.util.Set;
-
-// TODO: replace all instanceof statements with type == PieceType.{}
-
 public enum PieceType {
-    PAWN {
-        @Override
-        public Set<MovementType> getMovementTypes() {
-            return Set.of(MovementType.PAWN);
-        }
-    },
-    KNIGHT {
-        @Override
-        public Set<MovementType> getMovementTypes() {
-            return Set.of(MovementType.LSHAPED);
-        }
-    },
-    BISHOP {
-        @Override
-        public Set<MovementType> getMovementTypes() {
-            return Set.of(MovementType.DIAGONAL);
-        }
-    },
-    ROOK {
-        @Override
-        public Set<MovementType> getMovementTypes() {
-            return Set.of(MovementType.HORIZONTAL, MovementType.VERTICAL);
-        }
-    },
-    QUEEN {
-        @Override
-        public Set<MovementType> getMovementTypes() {
-            return Set.of(MovementType.HORIZONTAL, MovementType.VERTICAL, MovementType.DIAGONAL);
-        }
-    },
-    KING {
-        @Override
-        public Set<MovementType> getMovementTypes() {
-            return Set.of(MovementType.KING);
-        }
-    };
-
-    public abstract Set<MovementType> getMovementTypes();
+    PAWN ,
+    KNIGHT ,
+    BISHOP ,
+    ROOK ,
+    QUEEN ,
+    KING
 }
