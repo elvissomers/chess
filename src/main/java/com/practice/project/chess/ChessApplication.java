f3df6060f674e4a386ca3b78b7637807f555ed07
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-28 10:27:45 +0200

    Removed some comments

--- src/main/java/com/practice/project/chess/ChessApplication.java
+++ src/main/java/com/practice/project/chess/ChessApplication.java
@@ -5,18 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class ChessApplication {
-//    private static ApplicationContext applicationContext;
     public static void main(String[] args) {
         SpringApplication.run(ChessApplication.class, args);
-//        displayAllBeans();
-
     }
-
-//    public static void displayAllBeans() {
-//        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
-//        for(String beanName : allBeanNames) {
-//            System.out.println(beanName);
-//        }
-//    }
 
 }
