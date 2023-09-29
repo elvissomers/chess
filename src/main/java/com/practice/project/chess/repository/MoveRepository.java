ed1b70e5cdf5c6f05f94f64a5978cce813297aaf
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-29 15:54:04 +0200

    Remove unused queries

--- src/main/java/com/practice/project/chess/repository/MoveRepository.java
+++ src/main/java/com/practice/project/chess/repository/MoveRepository.java
@@ -5,17 +5,10 @@ import com.practice.project.chess.repository.entity.pieces.Piece;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MoveRepository extends JpaRepository<Move, Long> {
-
-    List<Move> findByPlayers_IdContaining(long id);
-
-    // TODO: split up in find move and find playermove
-    Optional<Move> findByNumberAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalTo(int number, int xFrom,
-                                                                                            int xTo, int yFrom, int yTo);
 
     Optional<Move> findByPieceAndTakenPieceAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalTo(Piece piece, Piece takenPiece,
                                                                                                         int xFrom, int xTo, int yFrom, int yTo);
