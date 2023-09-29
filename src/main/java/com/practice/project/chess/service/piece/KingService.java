161d760104b2b8b9ca4645ed597458e9736f81cd
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-09-29 15:43:16 +0200

    Removed dependencies from KingService.java (dependencies that KingService should not have)

--- src/main/java/com/practice/project/chess/service/piece/KingService.java
+++ src/main/java/com/practice/project/chess/service/piece/KingService.java
@@ -1,56 +1,32 @@
 package com.practice.project.chess.service.piece;
 
-import com.practice.project.chess.repository.entity.Game;
-import com.practice.project.chess.repository.entity.Player;
-import com.practice.project.chess.repository.entity.pieces.King;
 import com.practice.project.chess.repository.entity.pieces.Piece;
-import com.practice.project.chess.repository.entity.pieces.Rook;
-import com.practice.project.chess.service.AllUtil;
 import com.practice.project.chess.service.BoardService;
 import com.practice.project.chess.service.MoveOptionService;
-import com.practice.project.chess.service.game.GameService;
-import com.practice.project.chess.service.player.PlayerService;
 import com.practice.project.chess.service.structures.BoardMap;
-import com.practice.project.chess.service.structures.Coordinate;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class KingService {
 
     private final BoardService boardService;
-    private final GameService gameService;
-    private final PlayerService playerService;
     private final MoveOptionService moveOptionService;
-
-    private Piece piece;
-    private BoardMap board;
-    private Game game;
 
     private int xPos;
     private int yPos;
 
     public void setMovableSquares(Piece piece, long gameId) {
-        setup(piece, gameId);
+        getPosition(piece);
+        BoardMap board = boardService.getBoardMapForGame(gameId);
 
-        setKingBasicMoves();
-        setKingCastlingMoves();
+        setKingBasicMoves(piece, board);
     }
 
     public void setMovableSquaresWithBoard(Piece piece, BoardMap board) {
         getPosition(piece);
-        this.board = board;
-        setKingBasicMoves();
-    }
-
-    public void setup(Piece piece, long gameId) {
-        this.piece = piece;
-        board = boardService.getBoardMapForGame(gameId);
-        game = gameService.getGame(gameId);
-        getPosition(piece);
+        setKingBasicMoves(piece, board);
     }
 
     private void getPosition(Piece piece) {
@@ -58,7 +34,7 @@ public class KingService {
         yPos = piece.getVerticalPosition();
     }
 
-    public void setKingBasicMoves(){
+    private void setKingBasicMoves(Piece piece, BoardMap board){
         for (int x = xPos-1; x <= xPos+1; x++) {
             for (int y = yPos-1; y <= yPos+1; y++) {
                 if (moveOptionService.withinBoard(x, y)) {
@@ -66,43 +42,5 @@ public class KingService {
                 }
             }
         }
-    }
-
-    // TODO: is king in check should be moved to LegalMoveService!
-    public void setKingCastlingMoves() {
-        King king = (King) piece;
-        if (king.isInCheck() || king.isHasMoved())
-            return;
-
-        if (canCastleShort())
-            king.addMovableSquare(board.getCoordinateByPos(xPos+2, yPos));
-
-        if (canCastleLong()) {
-            king.addMovableSquare(board.getCoordinateByPos(xPos-2, yPos));
-        }
-    }
-
-    private boolean canCastleShort() {
-        return board.getPieceByPos(xPos+1,yPos) == null &&
-                kingIsSafeAt(board.getCoordinateByPos(xPos + 1, yPos)) &&
-                board.getPieceByPos(xPos+2, yPos) == null &&
-                board.getPieceByPos(xPos+3, yPos) instanceof Rook rook &&
-                !rook.isHasMoved();
-    }
-
-    private boolean canCastleLong() {
-        return board.getPieceByPos(xPos-1, yPos) == null &&
-                kingIsSafeAt(board.getCoordinateByPos(xPos - 1, yPos)) &&
-                board.getPieceByPos(xPos-2, yPos) == null &&
-                board.getPieceByPos(xPos-3, yPos) == null &&
-                board.getPieceByPos(xPos-4, yPos) instanceof Rook rook &&
-                !rook.isHasMoved();
-    }
-
-    public boolean kingIsSafeAt(Coordinate position) {
-        Player attackingPlayer = AllUtil.getOpponentPlayer(game, piece.getPlayer());
-        playerService.setAllAttackedAndMovableSquaresForPlayer(attackingPlayer);
-        List<Coordinate> attackedSquares = playerService.getAllAttackedSquaresForPlayer(attackingPlayer);
-        return !attackedSquares.contains(position);
     }
 }
