45f87c35f01d209952f204298f374f584d083411
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-04 09:52:59 +0200

    Setup DAO structure: Piece

--- src/main/java/com/practice/project/chess/repository/dao/pieces/PieceDao.java
+++ src/main/java/com/practice/project/chess/repository/dao/pieces/PieceDao.java
@@ -1,4 +1,32 @@
 package com.practice.project.chess.repository.dao.pieces;
 
+import com.practice.project.chess.repository.enums.PieceType;
+import com.practice.project.chess.repository.enums.Team;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="pieces")
 public class PieceDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int horizontalPosition;
+    private int verticalPosition;
+
+    @Enumerated(EnumType.STRING)
+    private Team team;
+
+    @Enumerated(EnumType.STRING)
+    private PieceType pieceType;
+
+    private boolean hasMoved;
 }
