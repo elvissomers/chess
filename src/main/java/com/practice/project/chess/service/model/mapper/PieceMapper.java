63d02e0d0aad87bb04be26f9db2d39650cedb5fa
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-04 10:02:52 +0200

    setup piece mapper

---
+++ src/main/java/com/practice/project/chess/service/model/mapper/PieceMapper.java
@@ -0,0 +1,9 @@
+package com.practice.project.chess.service.model.mapper;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+public class PieceMapper {
+}
