4d6b80d2d6aecacfca380cb4a8d78c84470ba25e
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-04 10:35:41 +0200

    Update repository to take DAO instead of entity

--- src/main/java/com/practice/project/chess/repository/PieceRepository.java
+++ src/main/java/com/practice/project/chess/repository/PieceRepository.java
@@ -1,5 +1,6 @@
 package com.practice.project.chess.repository;
 
+import com.practice.project.chess.repository.dao.pieces.PieceDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.practice.project.chess.service.model.pieces.Piece;
 import org.springframework.stereotype.Repository;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PieceRepository extends JpaRepository<Piece, Long> {
+public interface PieceRepository extends JpaRepository<PieceDao, Long> {
 
     Optional<Piece> findByHorizontalPositionAndVerticalPositionAndPlayer_Game_Id(
             int xPos, int yPos, long id);
