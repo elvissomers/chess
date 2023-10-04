e47c928d694f232ba3e8b7682086d317da5dc580
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-04 10:35:26 +0200

    Added coordinate back into Piece

--- src/main/java/com/practice/project/chess/controller/dto/mapper/PieceDtoMapper.java
+++ src/main/java/com/practice/project/chess/controller/dto/mapper/PieceDtoMapper.java
@@ -1,6 +1,7 @@
 package com.practice.project.chess.controller.dto.mapper;
 
 import com.practice.project.chess.controller.dto.PieceDto;
+import com.practice.project.chess.repository.dao.pieces.PieceDao;
 import com.practice.project.chess.service.model.pieces.Piece;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,12 @@ import org.springframework.stereotype.Component;
 @NoArgsConstructor
 public class PieceDtoMapper {
 
-    public PieceDto pieceToPieceDto(Piece piece) {
+    public PieceDto pieceToPieceDto(PieceDao piece) {
         return PieceDto.builder()
                 .horizontalPosition(piece.getHorizontalPosition())
                 .verticalPosition(piece.getVerticalPosition())
                 .pieceType(piece.getPieceType())
-                .team(piece.getPlayer().getTeam())
+                .team(piece.getTeam())
                 .build();
     }
 }
