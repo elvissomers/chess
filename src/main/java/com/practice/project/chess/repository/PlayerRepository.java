dc6b227aeaf5c276a936231db6e15a17e39b6105
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-04 16:37:10 +0200

    Change PlayerRepository.java datatype to DAO

--- src/main/java/com/practice/project/chess/repository/PlayerRepository.java
+++ src/main/java/com/practice/project/chess/repository/PlayerRepository.java
@@ -1,11 +1,7 @@
 package com.practice.project.chess.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.practice.project.chess.service.model.Player;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<PlayerDao, Long> {
 }
