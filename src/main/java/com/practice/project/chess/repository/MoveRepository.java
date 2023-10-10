b624efec0436eb21a9ea532c5654ac6913ef5ef3
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-10 15:17:32 +0200

    Open changes (work in progress)

--- src/main/java/com/practice/project/chess/repository/MoveRepository.java
+++ src/main/java/com/practice/project/chess/repository/MoveRepository.java
@@ -1,6 +1,7 @@
 package com.practice.project.chess.repository;
 
-import com.practice.project.chess.service.model.movehistory.Move;
+import com.practice.project.chess.repository.dao.MoveDao;
+import com.practice.project.chess.repository.enums.PieceType;
 import com.practice.project.chess.service.model.pieces.Piece;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,8 +9,13 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface MoveRepository extends JpaRepository<Move, Long> {
+public interface MoveRepository extends JpaRepository<MoveDao, Long> {
 
-    Optional<Move> findByPieceAndTakenPieceAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalTo(Piece piece, Piece takenPiece,
-                                                                                                        int xFrom, int xTo, int yFrom, int yTo);
+    // TODO : MoveDao has DAO piece attributes, so the query should use those instead of
+    // TODO - Piece.
+
+    // TODO 2: Should MoveDAO have PieceDAO - attributes at all?
+    // seems yes, this is the way of sql
+    Optional<MoveDao> findByPiece_IdAndTakenPiece_IdAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalToAndPromotedTo(long pieceId, long takenPieceId,
+                                                                                                                              int xFrom, int xTo, int yFrom, int yTo, PieceType promotedTo);
 }
--- src/main/java/com/practice/project/chess/service/logic/MoveService.java
+++ src/main/java/com/practice/project/chess/service/logic/MoveService.java
@@ -1,6 +1,7 @@
 package com.practice.project.chess.service.logic;
 
 import com.practice.project.chess.service.logic.game.util.MoveUtil;
+import com.practice.project.chess.service.model.mapper.MoveMapper;
 import com.practice.project.chess.service.model.movehistory.Move;
 import com.practice.project.chess.service.model.pieces.Piece;
 import com.practice.project.chess.repository.MoveRepository;
@@ -11,16 +12,19 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class MoveService {
-    // TODO: move should also be used as component only, and DAO should be used for obtaining from and saving to repository
 
     private final MoveRepository moveRepository;
 
     private final BoardService boardService;
 
+    private final MoveMapper moveMapper;
+
+    // TODO: after getting or creating a move, it should also be saved (in DAO form) to both MoveRepository and PlayerMoveRepository
     public Move getOrCreateMove(Piece piece, Coordinate destination, Piece takenPiece) {
-        return moveRepository.findByPieceAndTakenPieceAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalTo(
-                piece, takenPiece, piece.getHorizontalPosition(), destination.getXPos(), piece.getVerticalPosition(),
-                destination.getYPos())
+        return moveRepository.findByPiece_IdAndTakenPiece_IdAndHorizontalFromAndHorizontalToAndVerticalFromAndVerticalToAndPromotedTo(
+                piece.getId(), takenPiece.getId(), piece.getHorizontalPosition(), destination.getXPos(), piece.getVerticalPosition(),
+                destination.getYPos()) // TODO: this should also have promotedTo? Or can we still set promotedTo into the DAO later? THis might be multiple moves, with different promotedtos otherwise!
+                .map(moveMapper::daoToMove)
                 .orElse(createMove(piece, destination, takenPiece));
     }
 
