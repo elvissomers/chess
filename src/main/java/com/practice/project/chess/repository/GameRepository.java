fe55af90aeadfc457f4e1be2b3aaff4d8021772c
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-10 14:14:16 +0200

    Update GameRepository.java to DAO use

--- src/main/java/com/practice/project/chess/repository/GameRepository.java
+++ src/main/java/com/practice/project/chess/repository/GameRepository.java
@@ -1,9 +1,9 @@
 package com.practice.project.chess.repository;
 
-import com.practice.project.chess.service.model.Game;
+import com.practice.project.chess.repository.dao.GameDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<GameDao, Long> {
 }
