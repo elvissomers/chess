bf7f10740bb1dd0edef94279cf3542a525f81ae0
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-10 14:13:30 +0200

    Test setup

--- src/test/java/com/practice/project/chess/service/logic/BoardServiceTest.java
+++ src/test/java/com/practice/project/chess/service/logic/BoardServiceTest.java
@@ -1,12 +1,17 @@
 package com.practice.project.chess.service.logic;
 
 import com.practice.project.chess.repository.GameRepository;
+import com.practice.project.chess.repository.dao.GameDao;
+import com.practice.project.chess.service.model.Game;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,11 +26,14 @@ class BoardServiceTest {
 
     @Test
     void getBoardMapForGame() {
-        subject.getBoardMapForGame()
-    }
+        // Given
+        int mockId = 1;
+        Mockito.when(gameRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(Mockito.mock(Game.class)));
+        // When
+        subject.getBoardMapForGame(mockId);
 
-    void testnew() {
-        subject.
+        // Then
+
     }
 
     @Test
