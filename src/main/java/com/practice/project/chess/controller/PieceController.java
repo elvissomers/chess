cf41a32ebfa98d4a436083f92e648e70b5646b87
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-11 13:05:55 +0200

    Create endpoint for service to fill piece repository with initial Pieces

--- src/main/java/com/practice/project/chess/controller/PieceController.java
+++ src/main/java/com/practice/project/chess/controller/PieceController.java
@@ -1,12 +1,10 @@
 package com.practice.project.chess.controller;
 
 import com.practice.project.chess.controller.dto.SquaresDto;
+import com.practice.project.chess.repository.filling.FillPieceRepositoryService;
 import com.practice.project.chess.service.logic.piece.PieceService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -15,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PieceController {
 
     private final PieceService pieceService;
+    private final FillPieceRepositoryService fillPieceRepositoryService;
+
+    @PutMapping("fill_repo")
+    private void fillPieceRepository() {
+        fillPieceRepositoryService.fillPieceRepository();
+    }
 
     @GetMapping("movable_squares")
     private SquaresDto getMovableSquaresForPiece() {
