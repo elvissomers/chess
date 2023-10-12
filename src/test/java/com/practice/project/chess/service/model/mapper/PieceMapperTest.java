89b556558259bd7af635d1dadad984ef38c1c819
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-12 11:29:36 +0200

    Trying to get Mockito,mock to work in Test class (without succes)

--- src/test/java/com/practice/project/chess/service/model/mapper/PieceMapperTest.java
+++ src/test/java/com/practice/project/chess/service/model/mapper/PieceMapperTest.java
@@ -8,8 +8,10 @@ import com.practice.project.chess.service.structures.Coordinate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
