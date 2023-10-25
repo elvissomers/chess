bfeee553e3992253640787fa3cd7c390d8cd890b
elvissomers <33720951+elvissomers@users.noreply.github.com>
2023-10-25 10:03:53 +0200

    Made KingService.java stateless.

--- src/main/java/com/practice/project/chess/service/logic/piece/KingService.java
+++ src/main/java/com/practice/project/chess/service/logic/piece/KingService.java
@@ -3,7 +3,6 @@ package com.practice.project.chess.service.logic.piece;
 import com.practice.project.chess.service.logic.AllUtil;
 import com.practice.project.chess.service.model.pieces.Piece;
 import com.practice.project.chess.service.logic.BoardService;
-import com.practice.project.chess.service.logic.MoveOptionService;
 import com.practice.project.chess.service.structures.BoardMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,29 +12,21 @@ import org.springframework.stereotype.Service;
 public class KingService {
 
     private final BoardService boardService;
-    private final MoveOptionService moveOptionService;
-
-    private int xPos;
-    private int yPos;
 
     public void setMovableSquares(Piece piece, long gameId) {
-        getPosition(piece);
         BoardMap board = boardService.getBoardMapForGame(gameId);
 
         setKingBasicMoves(piece, board);
     }
 
     public void setMovableSquaresWithBoard(Piece piece, BoardMap board) {
-        getPosition(piece);
         setKingBasicMoves(piece, board);
     }
 
-    private void getPosition(Piece piece) {
-        xPos = piece.getHorizontalPosition();
-        yPos = piece.getVerticalPosition();
-    }
-
     private void setKingBasicMoves(Piece piece, BoardMap board){
+        int xPos = piece.getCoordinate().getXPos();
+        int yPos = piece.getCoordinate().getYPos();
+
         for (int x = xPos-1; x <= xPos+1; x++) {
             for (int y = yPos-1; y <= yPos+1; y++) {
                 if (AllUtil.withinBoard(x, y)) {
